97114b0499917a99cb8d74739dc3fc8ea09b3f6e
serhiibilous <serhiybilouss@gmail.com>
2019-08-25 22:57:02 +0300

    update app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express(),
-path = require('path'),
-publicDir = path.join(__dirname,'public');
+const express = require('express')
+const port = process.env.PORT || 3000
+const app = express()
+path = require('path')
+publicDir = path.join(__dirname,'public')
 
 app.use(express.static(publicDir))
 
-app.listen(port);
-module.exports = app;
+app.listen(port)
+module.exports = app
